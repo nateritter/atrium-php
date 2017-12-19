aa133e212eb548b92060c3b121458455e29d40ed
Nate Ritter <nate@perfectspace.com>
2017-12-19 12:24:42 -0800

    Users.

--- atrium/AtriumClient.php
+++ atrium/AtriumClient.php
@@ -3,6 +3,7 @@
 namespace NateRitter\AtriumPHP;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\RequestOptions;
 use NateRitter\AtriumPHP\Models\User;
 use NateRitter\AtriumPHP\Models\Member;
 use NateRitter\AtriumPHP\Models\Account;
@@ -49,5 +50,237 @@ class AtriumClient
         $this->environment = $environment;
         $this->mx_api_key = $mx_api_key;
         $this->mx_client_id = $mx_client_id;
+    }
+
+    public function updateUser(
+        $userGUID,
+        $identifier = '',
+        $is_disabled = true,
+        $metadata = ''
+    )
+    {
+        $inner = [];
+
+        if ($identifier != '') {
+            $inner['identifier'] = $identifier;
+        }
+
+        if (empty($is_disabled)) {
+            $inner['is_disabled'] = 'false';
+        } else {
+            $inner['is_disabled'] = 'true';
+        }
+
+        if ($metadata !== '') {
+            $inner['metadata'] = json_encode($metadata);
+        }
+
+        $outer['user'] = $inner;
+
+        $response = $this->makeRequest('PUT', '/users/' . $userGUID, $outer);
+        $response = json_decode($response);
+
+        return new User((array) $response->user);
+    }
+
+    /**
+     * Read the user account
+     * @param  string $userGUID
+     * @return \NateRitter\AtriumPHP\Models\User
+     */
+    public function readUser($userGUID)
+    {
+        $response = $this->makeRequest('GET', '/users/' . $userGUID, []);
+        return new User((array) json_decode($response)->user);
+    }
+
+    /**
+     * Delete a user by their GUID
+     * @param  string $userGUID
+     * @return string           JSON encoded string
+     */
+    public function deleteUser($userGUID)
+    {
+        return $this->makeRequest('DELETE', '/users/' . $userGUID, []);
+    }
+
+    /**
+     * List created users
+     * @param  string $page
+     * @param  string $records_per_page
+     * @return array
+     */
+    public function listUsers($page = '', $records_per_page = '')
+    {
+        $params = $this->optionalParameters('', '', '', $page, $records_per_page);
+
+        $response = $this->makeRequest('GET', '/users' . $params, []);
+        $parsedJSON = json_decode($response);
+        $JSONArray = (array) $parsedJSON->users;
+        $users = [];
+
+        foreach ($JSONArray as $user) {
+            $users[] = new User((array) $user);
+        }
+
+        return $users;
+    }
+
+    /**
+     * Create a new user
+     * @param  string $identifier
+     * @param  bool   $is_disabled
+     * @param  string $metadata    JSON encoded string
+     * @return \NateRitter\AtriumPHP\Models\User
+     */
+    public function createUser($identifier = '', $is_disabled = true, $metadata = '')
+    {
+        $inner = [];
+        $outer = [];
+
+        if ($identifier !== '') {
+            $inner['identifier'] = (string) $identifier;
+        }
+
+        if (empty($is_disabled)) {
+            $inner['is_disabled'] = 'false';
+        } else {
+            $inner['is_disabled'] = 'true';
+        }
+
+        if ($metadata !== '') {
+            $inner['metadata'] = json_encode($metadata);
+        }
+
+        $outer['user'] = $inner;
+
+        $response = $this->makeRequest('POST', '/users', $outer);
+        $response = json_decode($response);
+
+        return new User((array) $response->user);
+    }
+
+    /**
+     * Makes a request to the endpoint and retuns a JSON string
+     * @param  string $mode     API HTTP mode (GET, POST, etc)
+     * @param  string $endpoint
+     * @param  array  $body
+     * @return string           JSON string response or error
+     */
+    public function makeRequest($mode, $endpoint, array $body)
+    {
+        $conn = new Client([
+            'base_uri' => 'https://'.$this->environment.'/',
+            'accept' => 'application/vnd.mx.atrium.v1+json',
+            'content-type' => 'application/json',
+            'headers' => [
+                'MX-API-KEY'   => $this->mx_api_key,
+                'MX-CLIENT-ID' => $this->mx_client_id
+            ]
+        ]);
+
+        $response = $conn->request($mode, $endpoint, [
+            RequestOptions::JSON => $body
+        ]);
+
+        if ($error = $this->httpError($response->getStatusCode())) {
+            $conn = null;
+            return json_encode($error);
+        }
+
+        $contents = $response->getBody(true)->getContents();
+        $conn = null;
+
+        return $contents;
+    }
+
+    /**
+     * Echo and exit on HTTP error
+     * @param  int   $code HTTP status code
+     * @return mixed       Error string or false
+     */
+    public function httpError($code)
+    {
+        $error = null;
+
+        switch ($code) {
+            case 400:
+                $error = (string) $code . ' error: Required parameter is missing.';
+                break;
+            case 401:
+                $error = (string) $code . ' error: Invalid MX-API-KEY, MX-CLIENT-ID, or being used in wrong environment.';
+                break;
+            case 403:
+                $error = (string) $code . ' error: Requests must be HTTPS.';
+                break;
+            case 404:
+                $error = (string) $code . ' error: GUID / URL path not recognized.';
+                break;
+            case 405:
+                $error = (string) $code . ' error: Endpoint constraint not met.';
+                break;
+            case 406:
+                $error = (string) $code . ' error: Specify valid API version.';
+                break;
+            case 409:
+                $error = (string) $code . ' error: Object already exists.';
+                break;
+            case 422:
+                $error = (string) $code . ' error: Data provided cannot be processed.';
+                break;
+            case 500:
+            case 502:
+            case 504:
+                $error = (string) $code . ' error: An unexpected error occured on the MX system.';
+                break;
+            case 503:
+                $error = (string) $code . ' error: Please try again later. The MX Platform is currently being updated.';
+                break;
+        }
+
+        // Quit on 4XX or 5XX errors
+        if ((int) floor($code / 100) == 4 || (int) floor($code / 100) == 5) {
+            return $error;
+        }
+
+        return false;
+    }
+
+    /**
+     * Returns string from list of parameters
+     * @param  string $name
+     * @param  string $from_date
+     * @param  string $to_date
+     * @param  string $page
+     * @param  string $records_per_page
+     * @return string
+     */
+    public function optionalParameters(
+        $name = '',
+        $from_date = '',
+        $to_date = '',
+        $page = '',
+        $records_per_page = ''
+    )
+    {
+        $params = '?';
+
+        if ($name != '') {
+            $params .= 'name=' . $name . '&';
+        }
+        if ($from_date != '') {
+            $params .= 'from_date=' . $from_date . '&';
+        }
+        if ($to_date != '') {
+            $params .= 'to_date=' . $to_date . '&';
+        }
+        if ($page != '') {
+            $params .= 'page=' . $page . '&';
+        }
+        if ($records_per_page != '') {
+            $params .= 'records_per_page=' . $records_per_page . '&';
+        }
+
+        return substr($params, 0, -1);
     }
 }
--- tests/Unit/AtriumClientTest.php
+++ tests/Unit/AtriumClientTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use Faker\Factory;
 use Dotenv\Dotenv;
 use Tests\TestCase;
 use GuzzleHttp\Client;
@@ -12,9 +13,11 @@ class AtriumClientTest extends TestCase
     public $mx_api_key;
     public $mx_client_id;
     public $client;
+    public $faker;
 
     public function setUp()
     {
+        $this->faker = Factory::create();
         $this->mx_api_key   = getenv('MX_API_KEY');
         $this->mx_client_id = getenv('MX_CLIENT_ID');
 
@@ -26,7 +29,11 @@ class AtriumClientTest extends TestCase
     }
 
     public function tearDown() {
-        //
+        // Delete any users that were created
+        $users = $this->client->listUsers(1, 1000);
+        foreach ($users as $user) {
+            $this->client->deleteUser($user->guid);
+        }
     }
 
     /**
@@ -45,157 +52,261 @@ class AtriumClientTest extends TestCase
 
     public function testCreateUser()
     {
-        //
+        $identifier = ''; // Don't want collissions with already created users.
+        $is_disabled = $this->faker->randomElement([true, false]);
+        $metadata = [
+            'first_name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
+        ];
+
+        $result = $this->client->createUser($identifier, $is_disabled, $metadata);
+        $this->assertNotNull($result->guid);
+
+        return $result;
     }
 
     public function testReadUser()
     {
-        //
+        $createdUser = $this->testCreateUser();
+
+        $result = $this->client->readUser($createdUser->guid);
+        $this->assertNotNull($result->guid);
     }
 
     public function testUpdateUser()
     {
-        //
+        $createdUser = $this->testCreateUser();
+
+        $firstName = $this->faker->firstName;
+        $lastName = $this->faker->lastName;
+
+        $modifiedUser = $createdUser;
+        $modifiedUser->metadata = json_encode([
+            'first_name' => $firstName,
+            'last_name' => $lastName,
+        ]);
+
+        $result = $this->client->updateUser(
+            $createdUser->guid,
+            $createdUser->identifier,
+            $createdUser->is_disabled,
+            $modifiedUser->metadata
+        );
+
+        $decodedMetadata = json_decode($modifiedUser->metadata);
+
+        $this->assertEquals($createdUser->guid, $result->guid);
+        $this->assertEquals($createdUser->identifier, $result->identifier);
+        $this->assertEquals($createdUser->is_disabled, $result->is_disabled);
+        $this->assertNotEquals($createdUser->metadata, $result->metadata);
+        $this->assertEquals($decodedMetadata->first_name, $firstName);
+        $this->assertEquals($decodedMetadata->last_name, $lastName);
     }
 
     public function testListUsers()
     {
-        //
+        $num = $this->faker->numberBetween(1, 10);
+
+        for ($i=0; $i < $num; $i++) {
+            $this->testCreateUser();
+        }
+
+        $users = $this->client->listUsers();
+
+        $this->assertCount($num, $users);
     }
 
     public function testDeleteUser()
     {
-        //
+        $user = $this->testCreateUser();
+
+        $result = $this->client->deleteUser($user->guid);
+
+        $this->assertEmpty($result);
     }
 
-    # INSTITUTION
+    // # INSTITUTION
 
-    public function testListInstitution()
-    {
-        //
-    }
+    // public function testListInstitution()
+    // {
+    //     //
+    // }
 
-    public function testReadInstitution()
-    {
-        //
-    }
+    // public function testReadInstitution()
+    // {
+    //     //
+    // }
 
-    public function testReadInstitutionCredentials()
-    {
-        //
-    }
+    // public function testReadInstitutionCredentials()
+    // {
+    //     //
+    // }
 
-    # MEMBER
+    // # MEMBER
 
-    public function testCreateMember()
-    {
-        //
-    }
+    // public function testCreateMember()
+    // {
+    //     //
+    // }
 
-    public function testReadMember()
-    {
-        //
-    }
+    // public function testReadMember()
+    // {
+    //     //
+    // }
 
-    public function testUpdateMember()
-    {
-        //
-    }
+    // public function testUpdateMember()
+    // {
+    //     //
+    // }
 
-    public function testDeleteMember()
-    {
-        //
-    }
+    // public function testDeleteMember()
+    // {
+    //     //
+    // }
 
-    public function testListMembers()
-    {
-        //
-    }
+    // public function testListMembers()
+    // {
+    //     //
+    // }
 
-    public function testAggregateMember()
-    {
-        //
-    }
+    // public function testAggregateMember()
+    // {
+    //     //
+    // }
 
-    public function testReadMemberAggregationStatus()
-    {
-        //
-    }
+    // public function testReadMemberAggregationStatus()
+    // {
+    //     //
+    // }
 
-    public function testListMemberMFAChallenges()
-    {
-        //
-    }
+    // public function testListMemberMFAChallenges()
+    // {
+    //     //
+    // }
 
-    public function testResumeMemberAggregation()
-    {
-        //
-    }
+    // public function testResumeMemberAggregation()
+    // {
+    //     //
+    // }
 
-    public function testListMemberCredentials()
-    {
-        //
-    }
+    // public function testListMemberCredentials()
+    // {
+    //     //
+    // }
 
-    public function testListMemberAccounts()
-    {
-        //
-    }
+    // public function testListMemberAccounts()
+    // {
+    //     //
+    // }
 
-    public function testListMemberTransactions()
-    {
-        //
-    }
+    // public function testListMemberTransactions()
+    // {
+    //     //
+    // }
 
-    # ACCOUNT
+    // # ACCOUNT
 
-    public function testReadAccount()
-    {
-        //
-    }
+    // public function testReadAccount()
+    // {
+    //     //
+    // }
 
-    public function testListAccounts()
-    {
-        //
-    }
+    // public function testListAccounts()
+    // {
+    //     //
+    // }
 
-    public function testListAccountTransactions()
-    {
-        //
-    }
+    // public function testListAccountTransactions()
+    // {
+    //     //
+    // }
 
-    public function testReadTransaction()
-    {
-        //
-    }
+    // public function testReadTransaction()
+    // {
+    //     //
+    // }
 
-    public function testListTransactions()
-    {
-        //
-    }
+    // public function testListTransactions()
+    // {
+    //     //
+    // }
 
-    # CONNECT WIDGET
+    // # CONNECT WIDGET
 
-    public function testCreateWidget()
-    {
-        //
-    }
+    // public function testCreateWidget()
+    // {
+    //     //
+    // }
 
-    # CLIENT
+    // # CLIENT
 
-    public function testTestMakeRequest()
-    {
-        //
-    }
+    // public function testTestMakeRequest()
+    // {
+    //     //
+    // }
 
     public function testHttpError()
     {
-        //
+        // 4XX / 5XX error
+        $code = $this->faker->randomElement([
+            400, 401, 403, 404, 405, 406, 409, 422, 500, 502, 504, 503
+        ]);
+        $result = $this->client->httpError($code);
+        $this->assertNotEquals($result, false);
+
+        // Non 4XX / 5XX error
+        $code = $this->faker->numberBetween(100, 300);
+        $result = $this->client->httpError($code);
+        $this->assertEquals($result, false);
+
+        // No status code passed in
+        $this->expectException(\PHPUnit\Framework\Error\Warning::class);
+        $result = $this->client->httpError();
     }
 
     public function testOptionalParameters()
     {
-        //
+        // Setup
+        $name = $this->faker->word;
+        $from_date = $this->faker->date;
+        $to_date = $this->faker->date;
+        $page = $this->faker->randomDigitNotNull;
+        $records_per_page = $this->faker->randomDigitNotNull;
+
+        // Blank
+        $params = $this->client->optionalParameters();
+        $this->assertEquals($params, '');
+
+        // Name
+        $params = $this->client->optionalParameters($name);
+        $this->assertEquals($params, '?name=' . $name);
+
+        // From Date
+        $params = $this->client->optionalParameters('', $from_date);
+        $this->assertEquals($params, '?from_date=' . $from_date);
+
+        // To Date
+        $params = $this->client->optionalParameters('', '', $to_date);
+        $this->assertEquals($params, '?to_date=' . $to_date);
+
+        // Page
+        $params = $this->client->optionalParameters('', '', '', $page);
+        $this->assertEquals($params, '?page=' . $page);
+
+        // Records Per Page
+        $params = $this->client->optionalParameters('', '', '', '', $records_per_page);
+        $this->assertEquals($params, '?records_per_page=' . $records_per_page);
+
+        // All at the same time
+        $params = $this->client->optionalParameters(
+            $name,
+            $from_date,
+            $to_date,
+            $page,
+            $records_per_page
+        );
+        $str = '?name='.$name.'&from_date='.$from_date.'&to_date='.$to_date.'&page='.$page.'&records_per_page='.$records_per_page;
+        $this->assertEquals($params, $str);
     }
 
     # CONNECTIVITY
